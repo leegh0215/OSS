7302d4f4250438cff3788b98b730f9adc5b011e3
gunhui_ <130688392+leegh0215@users.noreply.github.com>
2024-12-06 22:58:35 +0900

    Create OSS.py and created a basic framework for the checklist.

---
+++ OSS.py
@@ -0,0 +1,43 @@
+import json
+
+#파일 저장 경로
+DO_FILE = "do.json"
+
+#초기 데이터 로드
+def load_do():
+  try:
+    with open(TODO_FILE, 'r') as file:
+      return json.load(file)
+  except FileNotFoundError:
+    return []
+
+#데이터 저장
+def save_do
+
+def main():
+  do_list = load_do()
+  while True:
+    print("\n[할 일 메모]")
+    print("1. 할 일 목록 보기")
+    print("2. 할 일 추가")
+    print("3. 할 일 완료 표시")
+    print("4. 할 일 삭제")
+    print("5. 종료")
+
+    choice = input("메뉴를 선택하세요 : ")
+    if choice == "1":
+      print("아직 미구현")
+    elif choice == "2":
+      print("아직 미구현")
+    elif choice == "3":
+      print("아직 미구현")
+    elif choice == "4":
+      print("아직 미구현")
+    elif choice == "5":
+      save_todo(todo_list):
+      print("프로그램을 종료합니다.")
+      break
+    else:
+      print("올바른 메뉴를 선택하세요!")
+
+main()
